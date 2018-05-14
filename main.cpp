de76dcdeca513c02e44feea7f85972f727c2a92f
Kirsty <leedomkirs@myvuw.ac.nz>
2018-05-14 16:03:12 +1200

    Merged with network code

--- main.cpp
+++ main.cpp
@@ -6,33 +6,30 @@
 double v_go=50;
 double left_bias = 1.15;
 double Kp=100;
-int section = 0;
+int quadrant = 1;
 
 void turn(double);
 void reverse();
 void stop();
+void openGate();
 void quadrant2();
 
 int main()
 {
     //test - just makes it go forward for 2 secs
 	init();
+    
+    //quadrant 1
+    openGate();
     set_threshold();
+    sleep1(1,0);
     
-    struct timeval t1;
-    struct timeval t2;
-    gettimeofday(&t1,0);
+    //quadrant 2
+    quadrant = 2;
     
-    while(1)
+    while(quadrant == 2)
     {
         quadrant2();
-		gettimeofday(&t2,0);
-		//break after 10 seconds
-		long elapsed = (t2.tv_sec -t1.tv_sec);
-		if(elapsed>20)
-		{
-			break;
-		}
 		sleep1(0,1000);
     }
     
@@ -47,7 +44,7 @@ void quadrant2() {
     //If it sees all white - it is on the next section (break for now)
     if(amount==ALL_WHITE)
     {
-        break;
+        quadrant = 3;
     }
     //If it sees no white - it is off course and will reverse
     else if(amount==NO_WHITE)
@@ -88,4 +85,16 @@ void reverse() {
 void stop(){
     set_motor(1,0);
     set_motor(2,0);
+}
+
+/**Open gate 1*/
+void openGate() {
+    char server_addr[15] = "130.195.6.196";
+	int port = 1024;
+	char message[24] = "Please";
+
+	connect_to_server(server_addr, port);
+	send_to_server(message);
+	receive_from_server(message);
+	send_to_server(message);
 }
