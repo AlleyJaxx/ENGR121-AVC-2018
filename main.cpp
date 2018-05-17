7549025cc5576dca63454d41dde3b6ee139800b7
Tomay0 <thomaspage33@gmail.com>
2018-05-17 19:33:47 +1200

    Improved debugging

--- main.cpp
+++ main.cpp
@@ -1,140 +1,51 @@
 #include <stdio.h>
 #include "E101.h"
+#include "quadrant3.h"
 #include <sys/time.h>
 
 double v_go=50;
 double left_bias = 1.15;
 double Kp=100;
 
-bool DEBUG = true;
-
-int white_threshold = 127;
-
 void turn(double);
+void reverse();
 void stop();
-void set_threshold();
-double get_turn();
+void quadrant3();
 
 int main()
 {
     //test - just makes it go forward for 2 secs
 	init();
-    set_threshold();
     
-    struct timeval t1;
-    struct timeval t2;
-    gettimeofday(&t1,0);
+    //quadrant 1
+    set_threshold();
+    sleep1(1,0);
     
     while(1)
     {
-        double amount = get_turn();
-        turn(amount);
-        
-		gettimeofday(&t2,0);
-		//break after 10 seconds
-		long elapsed = (t2.tv_sec -t1.tv_sec);
-		if(elapsed>10)
-		{
-			break;
-		}
-		sleep1(0,50);
+        quadrant3();
+		sleep1(0,1000);
     }
     
     stop(); 
 }
 
 /**
-* Sets the threshold value for checking white
-*/
-void set_threshold()
-{
-	//get picture
-    take_picture();
-	
-	if(DEBUG)
-	{
-		char filename[5] = "test";
-		save_picture(filename);
-	}
+ * Line following quadrant
+ */
+void quadrant3() {
+    double amount = doScan();
     
-    //determine minimum and maximum shades of white in the picture
-    int min_white = 255;
-    int max_white = 0;
-    
-    int y = 120;
-    for(int x=0;x<320;x++)
+    //If it sees no white - it is off course and will reverse
+    if(amount==NO_WHITE)
     {
-		int white = get_pixel(y,x,3);
-		if(white>max_white)
-		{
-			max_white = white;
-		}
-		if(white<min_white)
-		{
-			min_white = white;
-		}
-	}
-	//threshold
-	white_threshold = min_white+(max_white-min_white)/2;
-	
-	if(DEBUG)
-	{
-		printf("Thres: %d. Min=%d, Max=%d\n",white_threshold,min_white,max_white);
-	}
-}
-
-/**
- * This will return a value between -1 and 1 of where the white line is. 0 is the exact centre. 1 is all the way to right, -1 is all the way to left
- * */
-double get_turn()
-{
-	if(DEBUG)
-	{
-		printf("---------\n");
-	}
-	//get picture
-    take_picture();
-	
-    int white_pixels = 0;
-    double white_location = 0;
-    
-	int y = 120;
-    for(int x=0;x<320;x++)
+        reverse();
+    }
+    //Otherwise - turn based on the amount given
+    else
     {
-		double percent_location = (x-160)/160;//-1 = completely to left, 1 = completely to right, 0 = centre
-		int white = get_pixel(y,x,3);
-		//detect white
-		if(white>white_threshold)
-		{
-			white_pixels++;
-			if(DEBUG)
-			{
-				printf("white (%d) at x=%d. Loc=%d\n",white,x,percent_location);
-			}
-			white_location += percent_location;
-		}
-	}
-
-	//no white pixels
-	if(white_pixels==0)
-	{
-		if(DEBUG)
-		{
-			printf("no white\n");
-		}
-		return 0;
-	}
-	
-
-	//average location of the white pixels
-	double average_white_location = white_location/white_pixels;
-	
-	if(DEBUG)
-	{
-		printf("%d white. avg loc=%d\n",white_pixels,average_white_location);
-	}
-	
-	return average_white_location;
+        turn(amount);
+    }
 }
 
 /**
@@ -143,6 +54,17 @@ double get_turn()
 void turn(double amount){
     int left= (int)((v_go+Kp*amount)*left_bias);
     int right= (int)(v_go-Kp*amount);
+    set_motor(2,left);
+    set_motor(1,right);
+}
+
+/**
+ * Makes the robot go in reverse
+ * */
+void reverse() {
+    int left = -(v_go*left_bias);
+    int right = -(v_go);
+
     set_motor(2,left);
     set_motor(1,right);
 }
---
+++ quadrant3.cpp
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include "E101.h"
+#include "quadrant3.h"
+
+int detectedIntersection = 0;
+int timer = 0;
+int turn_value = 0;
+int white_threshold = 127;
+
+bool DEBUG = true;
+
+/**
+* Sets the threshold value for checking white
+*/
+void set_threshold()
+{	
+	//get picture
+    take_picture();
+	
+	if(DEBUG)
+	{
+		char filename[5] = "test";
+		save_picture(filename);
+	}
+    
+    //determine minimum and maximum shades of white in the picture
+    int min_white = 255;
+    int max_white = 0;
+    
+    int y = 120;
+    for(int x=0;x<320;x++)
+    {
+		int white = get_pixel(y,x,3);
+		if(white>max_white)
+		{
+			max_white = white;
+		}
+		if(white<min_white)
+		{
+			min_white = white;
+		}
+	}
+	//threshold
+	white_threshold = min_white+(max_white-min_white)/2;
+	
+	if(DEBUG)
+	{
+		printf("Thres: %d. Min=%d, Max=%d\n",white_threshold,min_white,max_white);
+	}
+}
+
+double doScan() {
+	take_picture();
+	
+	//calculate number of pixels on left side of screen and right
+	int white_pixels_left = 0;
+	int white_pixels_right = 0;
+	
+	double white_location = 0;
+	
+	int y = 120;
+	
+	for (int x = 0; x < 320; x++) {
+		
+		double percent_location = ((double)x - 160.0) / 160.0;
+		int white = get_pixel(y, x, 3);
+		
+		if (white > white_threshold){
+		
+			white_location += percent_location;
+			if (x < 160.0) {
+				white_pixels_left++;
+					
+			} else {	
+				white_pixels_right++;
+			}
+		}
+	
+	}
+
+	//whether all pixels on left or on right are white.
+	bool all_white_left = white_pixels_left > 157.0;
+	bool all_white_right = white_pixels_right > 157.0;
+	
+	//detects T intersection
+	if (all_white_left && all_white_right) {
+		timer = 0;
+		if(detectedIntersection!=1){
+			detectedIntersection = 1;
+			if(DEBUG) {
+				printf("T intersect\n");
+			}
+		}
+		return 0;
+	//Intersection on left only
+	} else if (all_white_left) {
+		timer = 0;
+		if(detectedIntersection==0){
+			detectedIntersection = 2;
+			if(DEBUG) {
+				printf("Left intersect\n");
+			}
+		}else if(detectedIntersection==3){
+			detectedIntersection=2;
+			if(DEBUG){
+				printf("T intersect\n");
+			}
+		}
+		return 0;
+	//Intersection on right only
+	} else if (all_white_right) {
+		timer = 0;
+		if(detectedIntersection==0){
+			detectedIntersection = 3;
+			if(DEBUG) {
+				printf("Right intersect\n");
+			}
+		}else if(detectedIntersection==2){
+			detectedIntersection=1;
+			if(DEBUG){
+                printf("T intersect\n");
+            }
+		}
+		return 0;
+	}
+	
+	//Detects only black - at the end of an intersection - turn the correct direction
+	if (white_pixels_left + white_pixels_right < 3) {
+		if(detectedIntersection!=0) {
+			//TURN RIGHT
+			if(detectedIntersection==3){
+				if(DEBUG){
+					printf("Turning Right\n");
+				}
+				turn_value = 0.4;
+			//TURN LEFT
+			}else if(detectedIntersection==2 || detectedIntersection==1){
+				if(DEBUG){
+					printf("Turning Left\n");
+				}
+				turn_value = -0.4;
+			//DEAD END
+			}else{
+				if(DEBUG){
+					printf("Dead end?\n");
+				}
+				turn_value = NO_WHITE;
+			}
+			detectedIntersection = 0;
+		}
+		//printf("All black\n");
+		return turn_value;
+	}
+	
+	//wants to go straight but intersection seen previously, keep going straight for n more checks
+	if(detectedIntersection!=0){
+		timer++;
+		//n checks before it continues following the line.
+		if(timer<15) {
+			if(DEBUG){
+				printf(".");
+			}
+			return 0;
+		}
+		if(DEBUG){
+			printf("Continuing forward\n");
+		}
+	}
+	//reset variables
+	detectedIntersection = 0;
+	turn_value = NO_WHITE;
+	//Go forward
+	//printf("Forward\n");
+	double average_white_location = white_location / (white_pixels_right + white_pixels_left);
+	
+	return average_white_location;
+}
