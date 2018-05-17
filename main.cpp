9605e9a318776820f658fc353d1ca2428d1f97d4
Alex (Study) <alexstudy@Alexs-MacBook-Pro-2.local>
2018-05-17 14:28:01 +1200

    Quad 3 first test

--- main.cpp
+++ main.cpp
@@ -1,24 +1,88 @@
 #include <stdio.h>
 #include "E101.h"
+#include "img_process.h"
+#include <sys/time.h>
 
-int main() {
+double v_go=50;
+double left_bias = 1.15;
+double Kp=100;
+int quadrant = 1;
+
+void turn(double);
+void reverse();
+void stop();
+void openGate();
+void quadrant2();
+
+int main()
+{
+    //test - just makes it go forward for 2 secs
 	init();
-	
-	int adc_reading = 0;
-	int adc_reading2 = 0;
-	int adc_reading3 = 0;
-	
-	for (int x = 1; x < 10; x = x + 1) {
-		adc_reading = read_analog(0);
-		adc_reading2 = read_analog(1);
-		adc_reading3 = read_analog(2);
-		
-		
-		sleep1(1, 0);
-		
-		printf("%d, %d, %d,", adc_reading, adc_reading2, adc_reading3);
-	
+    
+    //quadrant 1
+    openGate();
+    set_threshold();
+    sleep1(1,0);
+    
+    //quadrant 2
+    quadrant = 2;
+    
+    while(quadrant == 2)
+    {
+        quadrant2();
+		sleep1(0,1000);
+    }
+    
+    stop(); 
 }
 
-return 0;
+/**
+ * Line following quadrant
+ */
+void quadrant2() {
+    double amount = get_turn();
+    //If it sees all white - it is on the next section (break for now)
+    if(amount==ALL_WHITE)
+    {
+        quadrant = 3;
+    }
+    //If it sees no white - it is off course and will reverse
+    else if(amount==NO_WHITE)
+    {
+        reverse();
+    }
+    //Otherwise - turn based on the amount given
+    else
+    {
+        turn(amount);
+    }
+}
+
+/**
+ * Turns the robot
+ * */
+void turn(double amount){
+    int left= (int)((v_go+Kp*amount)*left_bias);
+    int right= (int)(v_go-Kp*amount);
+    set_motor(2,left);
+    set_motor(1,right);
+}
+
+/**
+ * Makes the robot go in reverse
+ * */
+void reverse() {
+    int left = -(v_go*left_bias);
+    int right = -(v_go);
+
+    set_motor(2,left);
+    set_motor(1,right);
+}
+
+/**
+ * Stops the robot
+ * */
+void stop(){
+    set_motor(1,0);
+    set_motor(2,0);
 }
---
+++ quadrant3.cpp
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include "E101.h"
+
+
+int detectedInstersection = 0;
+
+
+int white_threshold = 127;
+
+
+int NO_WHITE = 10;
+
+
+
+int endOfQuadrant = 11;
+
+
+bool DEBUG = false;
+
+/**
+* Sets the threshold value for checking white
+*/
+void set_threshold()
+{
+	//get picture
+    take_picture();
+	
+	if(DEBUG)
+	{
+		char filename[5] = "test";
+		save_picture(filename);
+	}
+    
+    //determine minimum and maximum shades of white in the picture
+    int min_white = 255;
+    int max_white = 0;
+    
+    int y = 120;
+    for(int x=0;x<320;x++)
+    {
+		int white = get_pixel(y,x,3);
+		if(white>max_white)
+		{
+			max_white = white;
+		}
+		if(white<min_white)
+		{
+			min_white = white;
+		}
+	}
+	//threshold
+	white_threshold = min_white+(max_white-min_white)/2;
+	
+	if(DEBUG)
+	{
+		printf("Thres: %d. Min=%d, Max=%d\n",white_threshold,min_white,max_white);
+	}
+}
+
+
+
+int main() {
+	init();
+	
+	
+	
+}
+
+double doScan() {
+	
+	take_picture();
+	
+	int white_pixels_left = 0;
+	int white_pixels_right = 0;
+	
+	double white_location = 0;
+	
+	int y = 120;
+	
+	for (int x = 0; x < 320; x++) {
+		
+		double percent_location = ((double)x - 160.0) / 160.0;
+		int white = get_pixel(y, x, 3);
+		
+		if (white > white_threshold){
+		
+			white_location += percent_location;
+			if (x < 160.0) {
+				white_pixels_left++;
+					
+			} else {	
+				white_pixels_right++;
+				
+			}
+			
+			
+			
+		}
+	
+	}
+
+	
+	bool all_white_left = white_pixels_left > 157.0;
+	
+	bool all_white_right = white_pixels_right > 157.0;
+		
+	
+	if (all_white_left && all_white_right) {
+		detectedInstersection = 1;
+		if(DEBUG){
+			printf("T\n");
+			
+		}
+		return 0;
+	} else if (all_white_left && !all_white_right) {
+		
+		detectedInstersection = 2;
+		printf("Left\n");
+		return 0;
+		
+	} else if (!all_white_left && all_white_right) {
+		
+		detectedInstersection = 3;
+		printf("Right\n");
+		return 0;
+	} else {
+		
+		detectedInstersection = 0;
+		printf("Forward\n");
+		
+	}
+	
+	if (all_white_left + all_white_right < 3) {
+		
+		return NO_WHITE;
+		
+		
+		
+	}
+	
+	double average_white_location = white_location / (all_white_right + all_white_left);
+	
+	return average_white_location;
+	
+		
+		
+	
+}
